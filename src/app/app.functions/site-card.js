719c4b3bf6517c6365d9e617c8b6700ff8c4962b
Paul Atlan <paulatlan@fastmail.fm>
2022-10-15 20:41:27 +0000

    use simple call first

--- src/app/app.functions/site-card.js
+++ src/app/app.functions/site-card.js
@@ -15,39 +15,61 @@ exports.main = async (context = {}, sendResponse) => {
   // Defines the first section of the CRM card
   // Defines variables for API endpoint and response data
   const resp = await hubspotClient.crm.contacts.basicApi.getById(hs_object_id);
-  const email = resp.properties.email
+  const data = resp.body;
+  const email = resp.body.properties.email
 
-  hubspotClient
-    .apiRequest({
-      method: 'GET',
-      path: `/crm/v4/objects/contacts/51/associations/companies`,
-    })
-    .then(associatedCompanies => {
-        console.log(associatedCompanies.body.results);
-        let companyId = associatedCompanies.body.results[0].toObjectId;
-        
-        // Defines how the returned data will be displayed
-        sendResponse({
-          sections: [{
-              type: "heading",
-              text: "Sample project custom CRM card"
-            },
-            {
-              type: "text",
-              text: `This card will retrieve data on the contact record. It is attached to contact id ${hs_object_id}.`
-            },
-            {
-              type: "text",
-              format: "markdown",
-              text: "This serverless function will retrieve a specific contact by ID."
-            },
-            {
-              type: "text",
-              format: "markdown",
-              text: `This contact's email is **${email}**`
-            },
-          ]
-        });
-      }
-    );
+  // hubspotClient
+  //   .apiRequest({
+  //     method: 'GET',
+  //     path: `/crm/v4/objects/contacts/51/associations/companies`,
+  //   })
+  //   .then(associatedCompanies => {
+  //       console.log(associatedCompanies.body.results);
+  //       let companyId = associatedCompanies.body.results[0].toObjectId;
+  //       
+  //       // Defines how the returned data will be displayed
+  //       sendResponse({
+  //         sections: [{
+  //             type: "heading",
+  //             text: "Sample project custom CRM card"
+  //           },
+  //           {
+  //             type: "text",
+  //             text: `This card will retrieve data on the contact record. It is attached to contact id ${hs_object_id}.`
+  //           },
+  //           {
+  //             type: "text",
+  //             format: "markdown",
+  //             text: "This serverless function will retrieve a specific contact by ID."
+  //           },
+  //           {
+  //             type: "text",
+  //             format: "markdown",
+  //             text: `This contact's email is **${email}**`
+  //           },
+  //         ]
+  //       });
+  //     }
+  //   );
+  sendResponse({
+    sections: [{
+        type: "heading",
+        text: "Sample project custom CRM card"
+      },
+      {
+        type: "text",
+        text: `This card will retrieve data on the contact record. It is attached to contact id ${hs_object_id}.`
+      },
+      {
+        type: "text",
+        format: "markdown",
+        text: "This serverless function will retrieve a specific contact by ID."
+      },
+      {
+        type: "text",
+        format: "markdown",
+        text: `This contact's email is **${email}**`
+      },
+    ]
+  });
 };
