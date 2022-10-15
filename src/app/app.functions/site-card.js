1838311032e99c48668c077e35f9d025662988a4
Paul Atlan <paulatlan@fastmail.fm>
2022-10-15 20:27:28 +0000

    body

--- src/app/app.functions/site-card.js
+++ src/app/app.functions/site-card.js
@@ -23,7 +23,7 @@ exports.main = async (context = {}, sendResponse) => {
       path: `/crm/v4/objects/contacts/51/associations/companies`,
     })
     .then(associatedCompanies => {
-        console.log(associatedCompanies);
+        console.log(associatedCompanies.body);
         let companyId = associatedCompanies.body.results[0].toObjectId;
         
         // Defines how the returned data will be displayed
